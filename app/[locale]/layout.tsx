0d7bdf5a12e6c222db07114706de43f5b07eeea0
Cristian <cristian.fermin-lopez.1@ens.etsmtl.ca>
2026-09-05 13:21:47 -0400

    test deploy vercel

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,9 @@
  * toute la logique métier (intl, auth, routes, etc.)
  */
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { NextIntlClientProvider } from 'next-intl';
@@ -19,9 +22,9 @@ export const metadata: Metadata = {
   title: 'MyApp',
 };
 
-export function generateStaticParams() {
+/*export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
-}
+}*/
 
 export default async function LocaleLayout({
   children,
